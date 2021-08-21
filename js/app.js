33a92c15b7e299c164bb15b2e3c3881823bddee6
Springhawk27 <sajjad0mahmud@gmail.com>
2021-08-21 15:21:53 +0600

    coupon function adjusted

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // mac section
 const macCost = document.getElementById('mac-cost');
 const macPrice = macCost.innerText;
-calculateTotl();
+calculateTotal();
 
 
 
@@ -10,21 +10,12 @@ const memory8GB = document.getElementById('8gb-cost');
 const memory16GB = document.getElementById('16gb-cost');
 
 memory8GB.addEventListener('click', function () {
-    updateMemoryCost('memory', memory8GB);
+    updateCost('memory', memory8GB);
 
 });
 memory16GB.addEventListener('click', function () {
-    updateMemoryCost('memory', memory16GB);
+    updateCost('memory', memory16GB);
 });
-
-
-function updateMemoryCost(productType, productID) {
-    const memoryCost = document.getElementById(productType + '-cost');
-    memoryCost.innerText = productID.value;
-
-    calculateTotl();
-};
-
 
 
 
@@ -34,24 +25,14 @@ const storage512GB = document.getElementById('512gb-cost');
 const storage1TB = document.getElementById('1tb-cost');
 
 storage256GB.addEventListener('click', function () {
-    updateMemoryCost('storage', storage256GB);
+    updateCost('storage', storage256GB);
 });
 storage512GB.addEventListener('click', function () {
-    updateMemoryCost('storage', storage512GB);
+    updateCost('storage', storage512GB);
 });
 storage1TB.addEventListener('click', function () {
-    updateMemoryCost('storage', storage1TB);
+    updateCost('storage', storage1TB);
 });
-
-// function updateStorageCost(storage) {
-//     const storageCost = document.getElementById('storage-cost');
-//     storageCost.innerText = storage.value;
-
-
-//     calculateTotl();
-
-// };
-
 
 
 
@@ -60,32 +41,36 @@ const deliveryFree = document.getElementById('delivery-free-cost');
 const delivryExpress = document.getElementById('delivery-express-cost');
 
 deliveryFree.addEventListener('click', function () {
-    updateMemoryCost('delivery', deliveryFree);
+    updateCost('delivery', deliveryFree);
 });
 delivryExpress.addEventListener('click', function () {
-    updateMemoryCost('delivery', delivryExpress);
+    updateCost('delivery', delivryExpress);
 });
 
-// function updateDeliveryCost(delivery) {
-//     const deliveryCost = document.getElementById('delivery-cost');
-//     deliveryCost.innerText = delivery.value;
-
-//     calculateTotl();
-
-// };
 
 
+// update cost function of the products - memory, storage and delivery cost upate
+function updateCost(productType, productID) {
+    const memoryCost = document.getElementById(productType + '-cost');
+    memoryCost.innerText = productID.value;
+
+    calculateTotal();
+};
 
 
 
 
+
+// get the product ID and parseFloat the containing value/text and then return the value
 function getProductValue(product) {
     const productID = document.getElementById(product + '-cost');
     const productCost = parseFloat(productID.innerText);
     return productCost;
-}
+};
 
-function calculateTotl() {
+
+// calculate the total cost
+function calculateTotal() {
     const macPrice = getProductValue('mac');
     const memoryPrice = getProductValue('memory');
     const storagePrice = getProductValue('storage');
@@ -93,8 +78,15 @@ function calculateTotl() {
     const totalPrice = macPrice + memoryPrice + storagePrice + deliveryPrice;
 
     document.getElementById('total-price').innerText = totalPrice;
-    document.getElementById('total').innerText = totalPrice;
+    const totalID = document.getElementById('total');
+    totalID.innerText = totalPrice;
+    getDiscount(totalPrice);
+};
 
+
+
+// add coupon and get 20% off
+function getDiscount(totalPrice) {
     const coupon = document.getElementById('coupon');
     coupon.addEventListener('click', function () {
         const couponCode = document.getElementById('coupon-code');
@@ -102,31 +94,13 @@ function calculateTotl() {
 
         if (couponValue == 'stevekaku') {
             document.getElementById('total').innerText = totalPrice - (totalPrice * (20 / 100));
-
         }
         else {
             document.getElementById('total').innerText = totalPrice;
-
         }
     })
 
-
-
-
 };
-
-// const coupon = document.getElementById('coupon');
-// coupon.addEventListener('click', function () {
-//     const couponCode = document.getElementById('coupon-code');
-//     const couponValue = couponCode.value;
-
-//     if (couponValue == 'stevekaku') {
-//         document.getElementById('total').innerText = totalPrice - (totalPrice * (20 / 100));
-
-//     }
-// })
-
-
 
 
 
